25bcc1ae170a1e66f6400fe0a3313bafc5d4b3ff
un5t0ppab13 <devbrights@gmail.com>
2015-12-22 13:56:19 -0800

    clean up whitespace

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 20151222195722) do
   create_table "posts", force: :cascade do |t|
     t.string   "title",       limit: 255
     t.text     "description", limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "email",       limit: 255
   end
 
