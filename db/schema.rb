5faeecd6f1580a0e779012b40d1b8ff10983f20f
un5t0ppab13 <devbrights@gmail.com>
2015-12-22 13:57:10 -0800

    now can migrate and rollback a t.string email in schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151222195722) do
+ActiveRecord::Schema.define(version: 20151222182150) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title",       limit: 255
     t.text     "description", limit: 65535
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "email",       limit: 255
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
   end
 
 end
