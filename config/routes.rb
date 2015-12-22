8582c5c589c0f57bbd0b4d5b7dbb88ba2e05afab
un5t0ppab13 <devbrights@gmail.com>
2015-12-22 11:11:57 -0800

    add bootstrap and stylesheet

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   # root 'welcome#index'
   # You can have the root of your site routed with "root"
-  # root 'post#index'
+  root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
